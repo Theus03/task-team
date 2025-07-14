af43bf7bb96a3de619968fc2e8f69ef5ac6232a2
Matheus Leite <matheusssleite@outlook.com>
2025-07-14 18:58:28 -0300

    Correção na estilização

--- src/App.tsx
+++ src/App.tsx
@@ -55,18 +55,18 @@ function App() {
         <button className='text-white bg-blue-500 rounded-2xl px-6 py-3 text-lg font-medium cursor-pointer transition ease-in duration-200 hover:bg-blue-600 hover:-translate-x-1'>+ Nova Tarefa</button>
       </div>
       <div className='px-32 py-2' id="board">
-        <div className='bg-blue-50 flex h-100 gap-6'>
-          <div id="pending" className='bg-amber-50 w-150 p-2 rounded'>
-            <div className='p-4 bg-orange-400 text-lg text-white font-medium rounded'>Pendente</div>
-            <div draggable className='p-6 m-6 bg-orange-200 cursor-move rounded shadow'>te</div>
+        <div className='bg-blue-50 flex h-100'>
+          <div id="pending" className='bg-amber-50 w-150 rounded'>
+            <div className='p-4 bg-orange-400 text-lg text-white font-medium rounded rounded-r-none'>Pendente</div>
+            <div draggable className='p-6 m-6 bg-orange-200 cursor-move rounded shadow'>Teste 1</div>
           </div>
-          <div id="progress" className='bg-blue-50 w-150 p-2 rounded'>
-            <div className='p-4 bg-blue-400 text-lg text-white font-medium rounded'>Em Andamento</div>
-            <div draggable className='p-6 m-6 bg-blue-200 cursor-move rounded shadow'>a</div>
+          <div id="progress" className='bg-blue-50 w-150 rounded'>
+            <div className='p-4 bg-blue-400 text-lg text-white font-medium rounded rounded-l-none rounded-r-none'>Em Andamento</div>
+            <div draggable className='p-6 m-6 bg-blue-200 cursor-move rounded shadow'>Teste 2</div>
           </div>
-          <div id="done" className='bg-green-50 w-150 p-2 rounded'>
-            <div className='p-4 bg-green-400 text-lg text-white font-medium rounded'>Concluído</div>
-            <div draggable className='p-6 m-6 bg-green-200 cursor-move rounded shadow'>b</div>
+          <div id="done" className='bg-green-50 w-150 rounded'>
+            <div className='p-4 bg-green-400 text-lg text-white font-medium rounded rounded-l-none'>Concluído</div>
+            <div draggable className='p-6 m-6 bg-green-200 cursor-move rounded shadow'>Teste 3</div>
           </div>
         </div>
       </div>
